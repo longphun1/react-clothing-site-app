671dde69b4650033f1ffa808464e9a523d4cc460
Long Phun <longphun90@yahoo.com>
2022-09-15 17:39:54 -0700

    added login form/auth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,11 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import {
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
@@ -12,6 +17,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign In with google
             </button>
+            <SignUpForm/>
         </div>
     )
 };
