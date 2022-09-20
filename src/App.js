b2a092e9cf4f43c1c0779576802cf23dde32cea3
Long Phun <longphun90@yahoo.com>
2022-09-19 20:48:56 -0700

    added cart icon and drop down menu

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
-const Shop = () => {
-  return <h1> I am shop page</h1>
-}
 // Make navigation component to be parent of the shop and home components that renders the categories
 // Index means it will render the component if it matches just the '/' path with nothing on it
 // Clicking on the shop link in the nav component will send you to the /shop route and render the shop component
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,18 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
+
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
+
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -27,7 +33,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropDown />}
             </div>
             <Outlet />
         </Fragment>
