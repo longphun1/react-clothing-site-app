58064124b7296bdb3aeaa54fc9b315b332878f26
Long Phun <longphun90@yahoo.com>
2022-09-20 21:24:58 -0700

    finalized checkout component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 // Make navigation component to be parent of the shop and home components that renders the categories
 // Index means it will render the component if it matches just the '/' path with nothing on it
@@ -13,7 +14,8 @@ const App = () => {
       <Route path='/' element={ <Navigation/> }> 
         <Route index element={ <Home /> } />
         <Route path='shop' element={ <Shop/> } />
-        <Route path='auth' element={ <Authentication/> }/>
+        <Route path='auth' element={ <Authentication/> } />
+        <Route path='checkout' element={ <Checkout /> } />
       </Route>
     </Routes>
   )
