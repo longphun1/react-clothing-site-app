90ca9ba9df0526d53a76dc40d011787736a36518
Long Phun <longphun90@yahoo.com>
2022-10-06 12:50:12 -0700

    added redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,40 @@
 import { compose, applyMiddleware, legacy_createStore as createStore} from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+};
 
-const composedEnchancers = compose(applyMiddleware(...middleWares));
+const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(rootReducer, undefined, composedEnchancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger,
+    sagaMiddleware
+].filter(Boolean);
+
+const composedEnchancer = (
+    process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+const composedEnchancers = composedEnchancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+    persistedReducer, 
+    undefined, 
+    composedEnchancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store)
