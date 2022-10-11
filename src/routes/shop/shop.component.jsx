0c39b4540d0945319be64ed13d98c527ee20c6cd
Long Phun <longphun90@yahoo.com>
2022-10-10 19:51:11 -0700

    added redux saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils.js";
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { setCategories } from '../../store/categories/category.action'
+import { fetchCategoriesStart } from '../../store/categories/category.action'
 
 const Shop = () => {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categories = await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categories));
-        };
-
-        getCategoriesMap();
-    }, [dispatch]);
+        dispatch(fetchCategoriesStart());
+    }, []);
 
     return (
         <Routes>
--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,3 +1,6 @@
 import { all, call } from 'redux-saga/effects';
+import { categoriesSage } from './categories/categories.saga';
 
-export function* rootSaga() {}
+export function* rootSaga() {
+    yield all([call(categoriesSage)])
+};
